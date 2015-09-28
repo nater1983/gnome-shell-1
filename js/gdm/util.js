11092fa9619d96f486cef0d92b278eb9a14fff91
Ray Strode <rstrode@redhat.com>
2015-09-28 19:56:53 -0400

    gdm: Only unlock with smartcard, if smartcard used for login
    If a smartcard is used for login, and the smartcard removal action
    is configured to lock the screen then we need to make sure the smartcard
    gets used for unlock (since otherwise you could ended up with a removed
    smartcard at an unlocked screen).

--- js/gdm/util.js
+++ js/gdm/util.js
@@ -641,7 +641,10 @@ export class ShellUserVerifier extends Signals.EventEmitter {
     }
 
     _getDetectedDefaultService() {
-        if (this._settings.get_boolean(PASSWORD_AUTHENTICATION_KEY))
+        if (this._smartcardManager.loggedInWithToken() &&
+            this._smartcardManager.lockOnRemoval())
+           return SMARTCARD_SERVICE_NAME;
+        else if (this._settings.get_boolean(PASSWORD_AUTHENTICATION_KEY))
             return PASSWORD_SERVICE_NAME;
         else if (this._smartcardManager)
             return SMARTCARD_SERVICE_NAME;
