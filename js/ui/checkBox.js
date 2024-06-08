scantist-ossops-m2/gnome-shell

checkBox: Reimplement styles in CSS
Stop using SVG assets other than the actual check icon. This will help
making it recolorable.

js/ui/checkBox.js
@@ -20,6 +20,11 @@ class CheckBox extends St.Button {
         this._box = new St.Bin({ y_align: Clutter.ActorAlign.START });
         container.add_actor(this._box);
 
+        this._check = new St.Icon({
+            icon_name: 'check-symbolic',
+        });
+        this._box.add_actor(this._check);
+
         this._label = new St.Label({ y_align: Clutter.ActorAlign.CENTER });
         this._label.clutter_text.set_line_wrap(true);
         this._label.clutter_text.set_ellipsize(Pango.EllipsizeMode.NONE);

nater1983/gnome-shell-1

js/ui/checkBox.js
@@ -23,6 +23,11 @@ class CheckBox extends St.Button {
         this._box = new St.Bin({y_align: Clutter.ActorAlign.START});
         container.add_child(this._box);
 
+        this._check = new St.Icon({
+            icon_name: 'check-symbolic',
+        });
+        this._box.set_child(this._check);
+
         this._label = new St.Label({y_align: Clutter.ActorAlign.CENTER});
         this._label.clutter_text.set_line_wrap(true);
         this._label.clutter_text.set_ellipsize(Pango.EllipsizeMode.NONE);